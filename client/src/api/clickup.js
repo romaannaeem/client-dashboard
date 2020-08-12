2b5b53dd85fefd53ccf3737c9cbe701db75fafad
Romaan <romaan567@hotmail.com>
2020-08-12 15:30:23 -0400

    added cors anywhere

--- client/src/api/clickup.js
+++ client/src/api/clickup.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://api.clickup.com/api/v2', //! Remember to add cors anywhere again! ~ https://cors-anywhere.herokuapp.com/https://api.clickup.com/api/v2
+  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.clickup.com/api/v2', //! Remember to add cors anywhere again! ~ https://cors-anywhere.herokuapp.com/https://api.clickup.com/api/v2
   headers: {
     Authorization: '1495561_c50210781cfaf81dd192910d5835967f670e3cc0',
   },
--- client/src/api/clientClickup.js
+++ client/src/api/clientClickup.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://api.clickup.com/api/v2', //! Remember to add cors anywhere again! ~ https://cors-anywhere.herokuapp.com/https://api.clickup.com/api/v2
+  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.clickup.com/api/v2', //! Remember to add cors anywhere again! ~ https://cors-anywhere.herokuapp.com/https://api.clickup.com/api/v2
   headers: {
     Authorization: 'pk_10562356_NUYQBFZ3D521WGB1FHFVO52LLSBCPF6Q',
   },
